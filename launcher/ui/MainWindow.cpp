973e305a663a24626ad25d7902db4a72161ff9c1
Matix732 <Welikepancakes@wp.pl>
2026-05-13 00:02:59 +0200

    fix try 21332122

--- launcher/ui/MainWindow.cpp
+++ launcher/ui/MainWindow.cpp
@@ -445,7 +445,7 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
             QUrl downloadUrl(myModpackUrl);
             
             InstanceImportTask* creationTask = new InstanceImportTask(downloadUrl, this, QMap<QString, QString>());
-            creationTask->setName("Mój Serwer");
+            creationTask->setName("MTcube smp");
             creationTask->setGroup("");
             creationTask->setIcon("default");
 
